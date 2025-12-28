40b68d68c84fe5cdeaf85ede71c09d6c22ca5601
Mouli <ursmouli@gmail.com>
2025-12-28 21:41:44 +0800

    basic login logout added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Input, Component, Output, EventEmitter } from '@angular/core';
+import { Input, Component, Output, EventEmitter, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { AuthService } from '../common/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -22,19 +23,29 @@ export class LoginComponent {
   @Input() error: string | null;
   @Output() submitEM = new EventEmitter();
 
+  authService = inject(AuthService);
+
   constructor() {
     this.error = '';
   }
 
   loginForm: FormGroup = new FormGroup({
-    username: new FormControl(''),
+    email: new FormControl(''),
     password: new FormControl(''),
   });
 
   onSubmit() {
     console.log(this.loginForm.value);
     console.log(this.loginForm.valid);
+
     if (this.loginForm.valid) {
+      // set role based on email for demo purpose
+      const role = this.loginForm.value.email.includes('admin') ? 'admin' : 'user';
+      this.authService.login(
+        this.loginForm.value.email, 
+        this.loginForm.value.password,
+        role
+      );
       this.submitEM.emit(this.loginForm.value);
     }
   }
