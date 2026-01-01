be0ca53a0ac367805b19d97b1ae7cd8f4e31c390
Mouli <ursmouli@gmail.com>
2026-01-02 00:43:27 +0800

    fontawsome dep added

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faXTwitter, faFacebook  } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  faFacebook = faFacebook;
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  faXTwitter = faXTwitter;
 }
